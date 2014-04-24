f99bba3758936b76d5d88f28e527e3a1a674724e
bite <mlisbit@gmail.com>
2014-04-24 03:04:26 -0400

    first commit

---
+++ static/main.js
@@ -0,0 +1,111 @@
+
+
+/* FOUNDATION ORBIT ===================================== */
+$(document).foundation({
+    orbit: {
+        slide_number: false,
+        animation_speed: 100,
+        bullets: false,
+    }
+});
+
+/* MIXITUP ============================================== */
+$(function(){
+    $('.row').mixItUp({
+        load: {
+            filter: '.web'
+        }
+    });
+});
+        
+/* MENU INTERACTIONS =============================== */
+$('.menu').css({width: 0})
+$('.contact').css({width: 0})
+
+$('.menu-button').click(function() {
+    if ($('.menu').css("width") === "0px") {
+        $('.contact').css("height","200px;");
+        $('.contact').animate({width: 0}, 100);
+        $('.menu').animate({width: 200}, 100);
+    } else {
+        $('.menu').animate({width: 0}, 100)
+        $('.contact').animate({width: 0}, 100);
+    }
+})
+$('.nav-contact-link').click(function() {
+    if ($('.contact').css("width") === "0px") {
+        $('.contact').css("height","auto");
+        $('.menu').animate({width: 0}, 100);
+        $('.contact').animate({width: 300}, 100);
+    } else {
+        $('.contact').animate({width: 0}, 100)
+    }
+})
+
+/* SCROLL TO ANCHOR ================================= */
+$('a[href*=#]:not([href=#])').click(function () {
+console.log("heyhey");
+if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    var target = $(this.hash);
+
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    console.log(target.selector);
+    if (target.length) {
+        $('html,body').animate({
+            scrollTop: target.offset().top
+        }, 300);
+        return false;
+    }
+}
+});
+            
+/* THUMBNAIL ANIMATION =============================== */
+$('.thumb').hover(function() {
+    $('.overlay', this).toggle()
+});
+
+/*
+$('a.orbit-prev').css('background', 'black');
+$('a.orbit-prev').css('opacity', '0.3');
+*/
+
+$('.orbit-next').click(function() {
+    console.log("CLICKED!");
+});
+
+/* SKROLLR ============================================= */
+( function( $ ) {
+
+    // Setup variables
+    $window = $(window);
+    $slide = $('.homeSlide');
+    $slideTall = $('.homeSlideTall');
+    $slideTall2 = $('.homeSlideTall2');
+    $body = $('body');
+    
+    // Init Skrollr
+    var s = skrollr.init({
+        render: function(data) {
+        
+            //Debugging - Log the current scroll position.
+            //console.log(data.curTop);
+        }
+    });
+    
+    // Get window size
+    winH = $window.height();
+    
+    // Keep minimum height 550
+    if(winH <= 550) {
+        winH = 550;
+    } 
+    
+    // Resize our slides
+    $slide.height(winH);
+    $slideTall.height(winH*2);
+    $slideTall2.height(winH*3);
+    
+    // Refresh Skrollr after resizing our sections
+    s.refresh($('.homeSlide'));
+        
+} )( jQuery );
